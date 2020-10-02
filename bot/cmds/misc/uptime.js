cba6c11a82878231402bdf240e2ae90b2aa5548d
the-patvocate <patroniton@gmail.com>
2020-10-02 09:44:41 -0700

    added time to uptime to make it a bit more useful

--- bot/cmds/misc/uptime.js
+++ bot/cmds/misc/uptime.js
@@ -17,7 +17,7 @@ module.exports = class Uptime extends Commando.Command {
       const start = this.client.patbot.startTime;
       const days = moment.duration(end.diff(start)).asDays();
 
-      message.channel.send(`I've been online since ${start.format('MMM Do')} (about ${Math.round(days)} days)`);
+      message.channel.send(`I've been online since ${start.format('MMM Do h:m A')} (about ${Math.round(days)} days)`);
     } catch (e) {
       console.log(e);
       message.reply('Sorry, something went wrong.');
