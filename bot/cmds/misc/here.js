45d0c81ae1f3eab43a71e5679014c4621aaff42a
the-patvocate <patroniton@gmail.com>
2020-10-05 22:26:36 -0700

    big brain idea

--- bot/cmds/misc/here.js
+++ bot/cmds/misc/here.js
@@ -16,7 +16,67 @@ module.exports = class Here extends Commando.Command {
   async run(message, args) {
     try {
       const user = await DatabaseResources.getUser(message.author.id);
-      const keywords = ['tonight', 'tomorrow', 'today', 'next', 'this', 'to']
+      const dayKeywords = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'u', 'm', 't', 'w', 'r', 'f', 's', 'tonight', 'tn', '2nite', '2night', 'tomorrow', 'tmr', 'tmrw'];
+      const modifiers = [
+        'next', // !here next friday, !here next Wednesday
+        'until', // !here until tomorrow, !here until Saturday
+        'this', // !here this friday, !here this week // actually not sure if this does anything
+        'to', // !here thursday to saturday, !here tonight to tomorrow
+        'and', // !here thursday and friday, !here friday and sunday // if and found terminate early and just set end to the start date and then treat this as a new argument
+      ];
+
+      const now = moment();
+
+      let availabilityWords = args.split(' ');
+
+      for (let availabilityWord of availabilityWords) {
+        let startDateFound = false;
+
+        let start = now.clone();
+        let end = now.clone();
+
+        // eg. !here thursday to friday, sunday to next wednesday
+        
+
+
+        // availabilityWords = availability.split(' ')
+        // check to see if the word is in either dayKeywords or modifiers array
+
+        // if not found, continue to next element (this word is ignored)
+        // availabilityWord is now contained in either dayKeywords or modifiers
+
+        // foreach dayKeywords
+          // if found
+            // if start not found (so this is likely our first date we've encountered, which will be assumed the start date)
+              // set startFound = true
+              // modify start date based on what the match was (ie. if tomorrow get the next day, !here friday gets the next friday)
+            // else (so start has been found, this is our end date)
+              // modify end date on what the match was (ie. if !here monday to saturday, start is on monday so you just get the next saturday after start)
+              // commit to DB
+              // reply
+              // return
+        
+        // foreach modifiers
+          // if found
+            // if start has NOT been found (ie. are we before or after start date. before = modifying start, after = modifying end)
+              // modify start date based on what the match was (ie. if !here next friday next would add a week)
+            // if start has been found
+              // modify end date based on what the match was (ie. if !here next friday next would add a week)
+        
+        
+        // outside of availabilityWords loop
+        // end date was never found, so user just said one day ie. !here friday
+        // set end date to start date
+        // commit to DB
+        // reply
+        // return
+      }
+
+
+
+
+
+
       let now = moment();
       let reply = 'Added availability for ';
       let availabilities = args.split(',');
