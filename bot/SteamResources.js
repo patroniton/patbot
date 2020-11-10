0d58dea068d4712dc8fe2da199850091b2e3c3b0
the-patvocate <patroniton@gmail.com>
2020-11-09 19:51:14 -0800

    added public add ability via searching by name or ID

--- bot/SteamResources.js
+++ bot/SteamResources.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch');
+const games = require('./../steam_games.json').applist.apps;
 const parseString = require('xml2js').parseString;
+const Fuse = require('fuse.js')
 
 async function getRssFeedForGame(id) {
   let json = null;
@@ -15,7 +17,46 @@ function parseUpdateIdFromRss(update) {
   return update.link[0].split('/').slice(-1)[0]
 }
 
+function findGameByName(name) {
+  const options = {
+    keys: ['name'],
+    includeScore: true,
+    ignoreLocation: true,
+    distance: 0,
+    threshold: 0.2
+  };
+
+  const index = Fuse.createIndex(options.keys, games);
+  const fuse = new Fuse(games, options, index);
+  const result = fuse.search(name);
+
+  if (result.length > 0 && result[0].score < 0.001) {
+    return result[0].item;
+  }
+
+  return result;
+}
+
+function findGameById(id) {
+  const options = {
+    keys: ['appid'],
+    threshold: 0
+  };
+
+  const index = Fuse.createIndex(options.keys, games);
+  const fuse = new Fuse(games, options, index);
+  const result = fuse.search(id);
+
+  if (result.length > 0) {
+    return result[0].item;
+  }
+
+  return result;
+}
+
 module.exports = {
-  getRssFeedForGame: getRssFeedForGame,
-  parseUpdateIdFromRss: parseUpdateIdFromRss
+  getRssFeedForGame,
+  parseUpdateIdFromRss,
+  findGameByName,
+  findGameById
 };
--- bot/cmds/steam_game_updates/add.js
+++ bot/cmds/steam_game_updates/add.js
@@ -1,7 +1,9 @@
 const Commando = require('discord.js-commando');
 const DatabaseResources = require('../../DatabaseResources.js');
 const constants = require('../../../env');
+const SteamResources = require('../../SteamResources.js');
 const PAT_USER_ID = constants['prod'].discord_ids.pat_user_id;
+const MAX_MATCHES = 10;
 
 module.exports = class Add extends Commando.Command {
   constructor(client) {
@@ -10,22 +12,50 @@ module.exports = class Add extends Commando.Command {
       group: 'steam_game_updates',
       memberName: 'add',
       description: 'Adds a steam game to the database.',
-      argsType: 'multiple'
     })
   }
 
   async run(message, args) {
     try {
-      if (message.author.id === PAT_USER_ID) {
-        const steamGameId = args.shift();
-        const name = args.join(' ');
-  
-        DatabaseResources.insertGame(steamGameId, name);
-        message.channel.send(`Added ${name} to the game list!`);
+      const name = args;
+      let game = null;
+
+      // adding by app ID
+      if (!isNaN(parseInt(name)) && typeof parseInt(name) === 'number') {
+        game = SteamResources.findGameById(name);
       } else {
-        message.channel.send(`Disabled for non-admins because of some abuse of the command`)
+        game = SteamResources.findGameByName(name);
       }
 
+      // multiple games were found - suggest 
+      if (Array.isArray(game)) {
+        let reply = '';
+
+        if (game.length === 0) {
+          reply = 'No matches found. Please double check the name of the game or add the game by the Steam ID.';
+        } else if (game.length <= MAX_MATCHES) {
+          reply = 'Found multiple results. Here are all the games I found:';
+          for (let singleGame of game) {
+            reply += `\n- ${singleGame.item.name} (ID: ${singleGame.item.appid})`;
+          }
+        } else {
+          reply = `Found more than ${MAX_MATCHES} potential matches (${game.length}). Please double check the name of the game or add the game by the Steam ID.`
+        } 
+
+        console.log(game);
+        message.channel.send(reply);
+        return;
+      }
+
+      const existingGame = await DatabaseResources.getSteamGame(game.appid);
+
+      if (existingGame) {
+        message.channel.send(`Whoops! We already have ${game.name} added to our game list`);
+        return;
+      }
+
+      DatabaseResources.insertGame(game.appid, game.name);
+      message.channel.send(`Added ${game.name} to the game list!`);
     } catch (e) {
       console.log(e);
       message.channel.send('Sorry, something went wrong.');
