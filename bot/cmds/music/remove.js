e7e649ca2b7635fab19f56005a43c0625eebb738
the-patvocate <patroniton@gmail.com>
2021-01-19 12:23:04 -0800

    added remove & song commands, fixed queue message ("[" and "]" characters were messing it up. replaced them for now, fix later if it's an issue)

--- bot/cmds/music/remove.js
+++ bot/cmds/music/remove.js
@@ -1,18 +1,18 @@
 const Commando = require('discord.js-commando');
 
-module.exports = class Debug extends Commando.Command {
+module.exports = class Remove extends Commando.Command {
   constructor(client) {
     super(client, {
-      name: 'debug',
+      name: 'remove',
       group: 'music',
-      memberName: 'debug',
-      description: 'Helps me make patbot better :).'
+      memberName: 'remove',
+      description: 'Removes a song. Currently only supports track number as an argument.'
     });
   }
 
   async run(message, args) {
     try {
-      this.client.patbot.musicPlayer.debug();
+      this.client.patbot.musicPlayer.remove(message, args);
     } catch (e) {
       console.log(e);
       message.channel.send('Sorry, something went wrong.');
